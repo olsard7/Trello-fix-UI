5042c2170e804160ede0eaf354dd4a776f6142d3
Oleg Sas <olsard7@gmail.com>
2023-06-18 17:25:35 +0300

    Working with concat and scss, comments remains

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const gulp = require('gulp');
+
 var concatCss = require('gulp-concat-css');
+var sass = require('gulp-sass')(require('sass'));
 
 gulp.task('default', function () {
     return gulp.src([
-        './src/reset.css',
-        './src/astyles.css',
+        './src/reset.scss',
+        './src/astyles.scss',
     ])
-      .pipe(concatCss('bundle.css'))
-      .pipe(gulp.dest('dest'));
+        .pipe(sass())
+        .pipe(concatCss('bundle.css'))
+        .pipe(gulp.dest('dest'));
   });
